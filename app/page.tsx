49882ed587e0ba31c2ba6309ef5fa5c13aaaa1ba
corbin murray <corbin.murray63@yahoo.com>
2025-03-19 13:33:53 -0500

    Horizontal alignment with container

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 import { Features } from "@/components/features";
-import { Footer } from "@/components/footer";
+
 import { Hero } from "@/components/hero";
-import { Navigation } from "@/components/navigation";
 
 export default function Home() {
 	return (
 		<main>
-			<Navigation />
+			
 			<Hero />
 			<Features />
-			<Footer />
+			
 		</main>
 	);
 }
