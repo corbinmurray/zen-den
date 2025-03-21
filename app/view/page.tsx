cde7dc964846be2ca29456ab4456f7116091af25
corbin murray <corbin.murray63@yahoo.com>
2025-03-21 18:54:45 -0500

    Fix padding

--- app/view/page.tsx
+++ app/view/page.tsx
@@ -1,9 +1,5 @@
 import { GardenViewer } from "@/components/garden/garden-viewer";
 
 export default async function ViewPage() {
-	return (
-		<main className="flex flex-col h-full w-full my-16">
-			<GardenViewer />
-		</main>
-	);
+	return <GardenViewer />;
 }
